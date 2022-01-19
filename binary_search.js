32dc259e25dee4d38a89c244f6fd323e02c4874f
Nasirdin <zhaparkulov02@gmail.com>
2022-01-19 21:18:28 +0600

    binary search

--- binary_search.js
+++ binary_search.js
@@ -47,7 +47,7 @@ const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
 //     let position = -1;
 //     while(found === false && start <= end){
 //         middle = Math.floor((start + end) / 2);
-//         if(array[middle] === i2tem){
+//         if(array[middle] === item){
 //             found = true;
 //             position = middle;
 //             return position;
